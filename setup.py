d692418b23bc51074019f4ea0f6cd14fd3cce07e
Rayson <raysonlcp1223@gmail.com>
2018-06-07 23:35:49 +0800

    Updated Setup Packages

--- setup.py
+++ setup.py
@@ -1,6 +1,9 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(name='gym_workflow',
+      packages=find_packages(),
       version='0.0.1',
-      install_requires=['gym', 'astropy', 'karellen-sqlite']
-)
+      install_requires=['gym', 'astropy', 'karellen-sqlite'],
+      dependecies_link=['https://github.com/pegasus-isi/montage-workflow-v2.git'],
+      include_package_data=True,
+      )
