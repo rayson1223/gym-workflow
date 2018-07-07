56a8cc1d0bf8535058254620bf78d1fbf436628b
Rayson <raysonlcp1223@gmail.com>
2018-07-08 01:23:06 +0800

    Added number of runs to 10 times for each configurations
    Signed-off-by: Rayson <raysonlcp1223@gmail.com>

--- gym_workflow/lib/montage/montage_generator.py
+++ gym_workflow/lib/montage/montage_generator.py
@@ -5,13 +5,14 @@ import time
 def main():
 	for cs in range(10):
 		for cn in range(10):
-			a = Montage()
-			a.build(cs + 1, cn + 1)
-			a.pegasus_run()
+			for i in range(10):
+				a = Montage()
+				a.build(cs + 1, cn + 1)
+				a.pegasus_run()
 
-			# Wait for the job submission status
-			time.sleep(5)
-			a.write_record(cs + 1, cn + 1)
+				# Wait for the job submission status
+				time.sleep(5)
+				a.write_record(cs + 1, cn + 1)
 
 	# a.pegasus_remove()
 
